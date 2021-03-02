9f3679a9cac93bcd6012413088576fd06b0488c2
katiusca Herrera Franco <katiusca.herrera@gmail.com>
2021-03-01 19:27:42 -0500

    Prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './component/input/input.component';
+import { ButtonComponent } from './component/button/button.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './module/login/login.component';
+import { HomeComponent } from './module/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    ButtonComponent,
+    MenuComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
